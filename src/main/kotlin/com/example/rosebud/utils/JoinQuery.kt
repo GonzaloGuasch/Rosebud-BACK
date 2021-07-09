726f6d49870f8e2da6b8dadbf37ba71b84224e4b
GonzaloGuasch <gonzaloguasch@outlook.com>
2021-07-09 11:11:25 -0400

    UserDTo

--- src/main/kotlin/com/example/rosebud/utils/JoinQuery.kt
+++ src/main/kotlin/com/example/rosebud/utils/JoinQuery.kt
@@ -4,15 +4,15 @@ object QueryConstants {
     const val QUERY =  "FROM movie " +
                              "INNER JOIN duration ON movie.duration_id = duration.id " +
                              "WHERE movie.title IN " +
-                                                  "(SELECT users_movies_watched.movies_watched_title " +
+                                                  "(SELECT users_movies_watched.movies_watched " +
                                                   "FROM users_movies_watched " +
                                                   "WHERE users_movies_watched.users_username = ?1) "
 
     const val QUERY_DISK =  "FROM disk " +
                             "INNER JOIN duration ON disk.duration_id = duration.id " +
                             "WHERE disk.title IN " +
-                            "(SELECT users_disk_listen.disk_listen_title " +
-                            "FROM users_disk_listen " +
-                            "WHERE users_disk_listen.users_username = ?1) "
+                                                "(SELECT users_disk_listen.disk_listen " +
+                                                "FROM users_disk_listen " +
+                                                "WHERE users_disk_listen.users_username = ?1) "
 
 }
